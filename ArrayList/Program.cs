9e9ca06981afa7aa6c3379dc74d84f92d842ca1a
hvu26510 <hvu26510@gmail.com>
2023-05-22 17:06:21 +0700

    Add project files.

---
+++ ArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayList01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //khai báo array list
+            ArrayList arl = new ArrayList();
+
+            arl.Add("nguyen van a");
+            arl.Add(10);
+            arl.Add(true);
+
+            Console.WriteLine("vi tri cua true: " + arl.IndexOf(true));
+
+            //Console.WriteLine(arl[0]);
+            //Console.WriteLine(arl[1]);
+            //Console.WriteLine(arl[2]);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DemoArrayList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arl = new ArrayList();
+
+            int count = 1;
+            while (true)
+            {
+                Console.WriteLine("Nhap phan thu thu: " + count);
+                Console.WriteLine("Nhap 'q' de dung ");
+                string str = Console.ReadLine();
+                if (str == "q") break;
+
+                arl.Add(int.Parse(str));
+            }
+
+            count = 0;
+
+            foreach(int phantu in arl)
+            {
+                if(phantu %2==0) count++;
+            }
+            Console.WriteLine($"So phan tu chan : {count}");
+
+            for(int i = 0; i < arl.Count; i++)
+            {
+                int x = Convert.ToInt32(arl[i].ToString());
+                if (x % 2 == 0)
+                {
+                    arl.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Sau khi loai so chan:");
+            foreach(int phantu in arl)
+            {
+                Console.Write(phantu +"     ");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
